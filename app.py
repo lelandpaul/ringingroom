6d143d67892ae7ed22672452c57b6ec9dc1ba3a8
Bryn Reinstadler <bryn.marie.r@gmail.com>
2020-03-21 10:34:54 -0400

    adding serverside room functionality and chat

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, join_room, leave_room
 from sassutils.wsgi import SassMiddleware
 import json
-
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 's7WUt93.ir_bFya7'
@@ -10,6 +9,7 @@ socketio = SocketIO(app)
 
 n_bells = 8
 global_bell_state = [True] * n_bells
+global_rooms = ["Advent","Old North"]
 
 # set up automatic sass compilation
 app.wsgi_app = SassMiddleware(app.wsgi_app, {
@@ -18,20 +18,59 @@ app.wsgi_app = SassMiddleware(app.wsgi_app, {
 
 
 # Serve the basic template
-@app.route('/',methods=('GET','POST'))
+@app.route('/', methods=('GET', 'POST'))
 def index():
-	return render_template('ringing_room.html')
+    return render_template('ringing_room.html')
 
 
 @socketio.on('pulling_event')
-def broadcast_ringing(event_dict):
-	cur_bell = event_dict["bell"]
-	if global_bell_state[cur_bell-1] is event_dict["stroke"]:
-		global_bell_state[cur_bell-1] = not global_bell_state[cur_bell-1]
-	else:
-		print('Current stroke disagrees between server and client')
-	emit('ringing_event',{"global_bell_state": global_bell_state, "who_rang": cur_bell},broadcast=True,include_self=True)
+def on_pulling_event(event_dict):
+    cur_bell = event_dict["bell"]
+    if global_bell_state[cur_bell - 1] is event_dict["stroke"]:
+        global_bell_state[cur_bell - 1] = not global_bell_state[cur_bell - 1]
+    else:
+        print('Current stroke disagrees between server and client')
+    emit('ringing_event', {"global_bell_state": global_bell_state, "who_rang": cur_bell},
+         broadcast=True, include_self=True)
+
+
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
+
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
+
+
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    emit(username + ' has entered the room.', room=room,
+         broadcast=True, include_self=True)
+
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    emit(username + ' has left the room.', room=room,
+         broadcast=True, include_self=True)
+
+
+@socketio.on('new_room')
+def new_room(data):
+    global_rooms.append(data['new_room_name'])
+    room = data['new_room_name']
+    username = data['username']
+    join_room(data['new_room_name'])
+    # Notification about new user joined room
+    send({"msg": username + " has created the " + room + " room."}, room=room)
 
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
