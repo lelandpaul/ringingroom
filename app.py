cb36e9c33cf8fac03e2b568783f3c5d82dce1c90
Bryn Reinstadler <bryn.marie.r@gmail.com>
2020-03-21 11:00:30 -0400

    fix strip_extension

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask_socketio import SocketIO, emit
 from sassutils.wsgi import SassMiddleware
 import json
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 's7WUt93.ir_bFya7'
 socketio = SocketIO(app)
@@ -13,25 +12,31 @@ global_bell_state = [True] * n_bells
 
 # set up automatic sass compilation
 app.wsgi_app = SassMiddleware(app.wsgi_app, {
-    'app': ('static/sass', 'static/css', '/static/css')
+    'app': {
+        'sass_path': 'static/sass',
+        'css_path': 'static/css',
+        'wsgi_path': '/static/css',
+        'strip_extension': True
+    }
 })
 
 
 # Serve the basic template
-@app.route('/',methods=('GET','POST'))
+@app.route('/', methods=('GET', 'POST'))
 def index():
-	return render_template('ringing_room.html')
+    return render_template('ringing_room.html')
 
 
 @socketio.on('pulling_event')
 def broadcast_ringing(event_dict):
-	cur_bell = event_dict["bell"]
-	if global_bell_state[cur_bell-1] is event_dict["stroke"]:
-		global_bell_state[cur_bell-1] = not global_bell_state[cur_bell-1]
-	else:
-		print('Current stroke disagrees between server and client')
-	emit('ringing_event',{"global_bell_state": global_bell_state, "who_rang": cur_bell},broadcast=True,include_self=True)
+    cur_bell = event_dict["bell"]
+    if global_bell_state[cur_bell - 1] is event_dict["stroke"]:
+        global_bell_state[cur_bell - 1] = not global_bell_state[cur_bell - 1]
+    else:
+        print('Current stroke disagrees between server and client')
+    emit('ringing_event', {"global_bell_state": global_bell_state, "who_rang": cur_bell}, broadcast=True,
+         include_self=True)
 
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
