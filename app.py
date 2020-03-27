ab8c30c575a5a5f782abe23e5103193c86a5c49c
lelandpaul <lelandpaul@gmail.com>
2020-03-27 17:03:15 -0400

    Tower class

--- app.py
+++ app.py
@@ -7,10 +7,39 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 's7WUt93.ir_bFya7'
 socketio = SocketIO(app)
 
-n_bells = 8
-# global_bell_state = [True] * n_bells
-global_room_names = ["Advent","Old North"]
-global_rooms = {name : [True]*n_bells for name in global_room_names}
+
+class Tower:
+	def __init__(self,name,n=8):
+		self._name = name
+		self._n = 8
+		self._bell_state = [True] * n
+	
+	@property
+	def name():
+		return(self._name)
+
+	@name.setter
+	def name(new_name):
+		self._name = new_name
+	
+	@property
+	def n_bells():
+		return(self._n)
+
+	@n_bells.setter
+	def n_bells(new_size):
+		self._n = new_size
+		self._bell_state = [True] * new_size
+
+	@property
+	def bell_state():
+		return(self._bell_state)
+
+	@bell_state.setter
+	def bell_state(new_state):
+		self._bell_state = new_state
+
+global_rooms = {'main': Tower('Ringing Room')}
 
 # set up automatic sass compilation
 app.wsgi_app = SassMiddleware(app.wsgi_app, {
@@ -27,6 +56,14 @@ app.wsgi_app = SassMiddleware(app.wsgi_app, {
 @app.route('/', methods=('GET', 'POST'))
 def index():
     return render_template('ringing_room.html')
+
+# Create / find other towers/rooms
+@app.route('/tower/<tower_code>')
+def tower(tower_code):
+	if tower_code not in global_rooms:
+		global_rooms[tower_code] = Tower(tower_code)
+	join_room(tower_code)
+	return render_template('ringing_room.html')
 
 
 @socketio.on('pulling_event')
@@ -60,7 +97,7 @@ def on_join(data):
     join_room(room)
     emit(username + ' has entered the room.', room=room,
          broadcast=True, include_self=True)
-    emit('global_state',{'global_bell_state': global_rooms[room]})
+    emit('global_state',{'global_bell_state': global_rooms[room].bell_state})
 
 
 @socketio.on('leave')
