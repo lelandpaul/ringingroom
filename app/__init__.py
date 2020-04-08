46417dfdeef61c299f32158a63aa1c227b12af83
lelandpaul <lelandpaul@gmail.com>
2020-04-08 13:16:59 -0400

    using flask_assets for cache-busting

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_socketio import SocketIO
-from sassutils.wsgi import SassMiddleware
+from flask_assets import Environment, Bundle
 from config import Config
 import os
 
@@ -13,16 +13,29 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 socketio = SocketIO(app)
+assets = Environment(app)
 
-# set up automatic sass compilation
-app.wsgi_app = SassMiddleware(app.wsgi_app, {
-    'app': {
-        'sass_path': 'static/sass',
-        'css_path': 'static/css',
-        'wsgi_path': 'static/css',
-        'strip_extension': False
-    }
-})
+
+# asset bundles
+bundles = {
+
+    'js_landing':   Bundle( 'landing.js',
+                            filters='jsmin', 
+                            output='gen/landing.%(version)s.js'),
+
+    'js_rr':        Bundle('ringing_room.js',
+                           'audio.js',
+                           # filters='jsmin',
+                            output='gen/rr.%(version)s.js'),
+
+    'css_static':   Bundle( 'css/static.css',
+                            output='gen/static.%(version)s.css'),
+
+    'css_rr':   Bundle( 'css/ringing_room.css',
+                            output='gen/rr.%(version)s.css'),
+}
+
+assets.register(bundles)
 
 from app.models import *
 
