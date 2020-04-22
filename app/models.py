ebd7a9700f132f850e791fe809eec496499fa260
lelandpaul <lelandpaul@gmail.com>
2020-04-22 12:53:59 -0400

    added display_name to user db

--- app/models.py
+++ app/models.py
@@ -10,6 +10,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    display_name = db.Column(db.String(64))
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
