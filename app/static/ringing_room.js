e0a527004b403957d133e8f9a78a02cc24787a46
lelandpaul <lelandpaul@gmail.com>
2020-05-17 18:42:36 -0400

    make sure to send all the keys needed

--- app/static/ringing_room.js
+++ app/static/ringing_room.js
@@ -44,6 +44,7 @@ var leave_room = function(){
     socketio.emit('c_user_left',
           {user_name: window.tower_parameters.cur_user_name, 
            user_token: window.tower_parameters.user_token,
+           anonymous_user: window.tower_parameters.anonymous_user,
           tower_id: cur_tower_id});
 };
 
