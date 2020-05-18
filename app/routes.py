ecdcf1754a022d5e7ebdeb604491d61f5078d7b7
lelandpaul <lelandpaul@gmail.com>
2020-05-17 20:24:48 -0400

    get rid of 8080

--- app/routes.py
+++ app/routes.py
@@ -19,7 +19,7 @@ def get_server_ip(tower_id):
     if not servers:
         return request.url_root
     else:
-        return 'http://' + servers[tower_id % len(servers)] + ':8080'
+        return 'https://' + servers[tower_id % len(servers)]
 
 # redirect for static files on subdomains
 
