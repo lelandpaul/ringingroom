780c38f4b3303fbdf18cc8d0de8657bac51463e7
lelandpaul <lelandpaul@gmail.com>
2020-08-31 11:59:52 -0400

    user registration endpoint

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,9 +1,13 @@
-from flask import jsonify
+from flask import jsonify, request, url_for
 from app.models import User, TowerDB, get_server_ip
+from app.extensions import db
 from app.api import bp
-from app.api.errors import error_response
+from app.api.errors import error_response, bad_request
 from app.api.auth import token_auth
 from flask_login import current_user
+
+
+# GET endpoints
 
 @bp.route('/user', methods=['GET'])
 @token_auth.login_required
@@ -41,6 +45,25 @@ def get_tower_settings(tower_id):
         'hosts': [u.to_dict() for u in tower.hosts],
     }
     return jsonify(data)
+
+
+# POST endpoints
+
+@bp.route('/user', methods=['POST'])
+def create_user():
+    data = request.get_json() or {}
+    if 'username' not in data or 'email' not in data or 'password' not in data:
+        return bad_request('Must include username, email, and password fields.')
+    if User.query.filter_by(username=data['email']).first():
+        return bad_request('There is already an account registered with that email address.')
+    user = User(username = data['username'], email = data['email'])
+    user.set_password(data['password'])
+    db.session.add(user)
+    db.session.commit()
+    response = jsonify(user.to_dict())
+    response.status_code = 201
+    response.headers['Location'] = url_for('api.get_user')
+    return response
 
 
 
