5b8ce0a97361d4ba388ff487dac1bd24394a7ff9
lelandpaul <lelandpaul@gmail.com>
2020-09-04 14:17:56 -0400

    Fixed problem where s_user_left might have the wrong payload

--- app/listeners.py
+++ app/listeners.py
@@ -93,7 +93,8 @@ def on_join(json):
         if current_user.username in tower.users.keys():
             log('SETUP User already present')
             tower.remove_user(current_user.id)
-            emit('s_user_left', {'user_name': current_user.username},
+            emit('s_user_left', {'user_id': current_user.id},
+                                 'username': current_user.username},
                                 broadcast = True,
                                 include_self = True,
                                 room = tower_id)
