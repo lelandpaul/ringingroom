6f0adfd2cf2a25853ffb4e886e3dd4d66b63e622
lelandpaul <lelandpaul@gmail.com>
2020-09-21 15:40:11 -0400

    temporarily disable assigned bell list

--- app/static/ringing_room.js
+++ app/static/ringing_room.js
@@ -1082,7 +1082,7 @@ $(document).ready(function() {
     @click="select_user"
     >
     [[ username ]] 
-        <span class="float-right pt-1" style="font-size: smaller;">
+        <span v-show="false" class="float-right pt-1" style="font-size: smaller;">
               [[assigned_bell_string]]
         </span>
 </li>
