c11595f4dfa5cec0e38a523f17b60a0ee02e78a5
lelandpaul <lelandpaul@gmail.com>
2021-01-08 20:09:40 -0500

    reverting handbell sounds

--- app/static/audio/processed_audio/hand/lowpass.py
+++
@@ -1,9 +0,0 @@
-from pydub import AudioSegment
-from pydub.effects import low_pass_filter
-from glob import glob
-
-for f in glob('*.wav'):
-    a = AudioSegment.from_wav(f)
-    lp = low_pass_filter(a, 1000)
-    a.export(f, format='wav')
-    
